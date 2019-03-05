ac4d6d3faf4ac45814ffd88f3ea34d4cb1fcc54c
Brandon Booth <brandon.m.booth@gmail.com>
2019-03-04 18:00:15 -0800

    Added optimal continuous trapezoidal segmented regression code

--- src/trapezoidal_segmented_regression/continuous_opt.py
+++ src/trapezoidal_segmented_regression/continuous_opt.py
@@ -11,13 +11,9 @@ import cvxopt
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error
 from itertools import combinations
 
-#num_segments = 2
-num_segments = 38
-#solver = 'quadprog'
-solver = 'cvxopt'
+num_segments = 9
 show_debug_plots = False
 
 def GetCVXOPTMatrix(M):
@@ -30,44 +26,21 @@ def GetCVXOPTMatrix(M):
    coo = M.tocoo()
    return cvxopt.spmatrix(coo.data.tolist(), coo.row.tolist(), coo.col.tolist(), size=M.shape)
 
-def GetConstraints(knots, n, start_constant=True):
-   G = None
-   h = None
-   A = np.zeros((n,n))
-   b = np.zeros(n)
-
-   # Add a fake knot at the end
-   knots.append(n-1)
-
-   last_knot = 0
-   for idx in range(len(knots)):
-      is_const_constraint = (((idx%2)==0) and start_constant) or (((idx%2)==1) and not start_constant)
-      if is_const_constraint:
-         interval_points = range(last_knot,knots[idx])
-         for x in interval_points:
-            A[x,x] = 1
-            A[x,x+1] = -1
-            b[x] = 0
-      else:
-         interval_points = range(last_knot,knots[idx]-1)
-         for x in interval_points:
-            A[x,x] = 1
-            A[x,x+1] = -2
-            A[x,x+2] = 1
-            b[x] = 0
-      last_knot = knots[idx]
-
-   zero_row_idx = np.all(A == 0, axis=1)
-   A = A[~zero_row_idx]
-   b = b[~zero_row_idx]
-
-   return G,h,A,b
-
-def LossFunction(x_pred, x_true):
-   return mean_squared_error(x_true, x_pred)
+def FitConstantSegment(signal):
+   if len(signal) > 1:
+      b = np.mean(signal.iloc[1:])
+      residuals = signal.iloc[1:] - b*np.ones(len(signal)-1)
+      loss_value = np.dot(residuals,residuals)
+   elif len(signal) == 1:
+      b = signal.iloc[0]
+      loss_value = 0.0
+   else:
+      b = np.nan
+      loss_value = np.inf
+   return b, loss_value
 
 def FitLineSegment(signal):
-   if len(signal) > 1:
+   if len(signal) > 0:
       try:
          M = np.vstack((signal.index, np.ones(len(signal)))).T
          coefs, residuals, rank, singular_vals = np.linalg.lstsq(M, signal.values, rcond=None)
@@ -86,21 +59,62 @@ def FitLineSegment(signal):
       average_error = np.inf
    return a, b, average_error
 
+def FitConstantSegmentWithIntersection(signal, i, j, a, b, x1, x2):
+   if np.isnan(a) or np.isnan(b):
+      return np.nan, np.nan, np.inf
+
+   x = np.array(signal.iloc[i:j+1].index)
+   y = np.array(signal.iloc[i:j+1])
+   P = np.array([len(x)]).astype(float)
+   q = np.array([-np.sum(y)]).astype(float)
+   G = np.array([[1], [-1]]).astype(float)
+   h = np.array([a*x2+b, -a*x1-b]).reshape(-1,1).astype(float)
+   if a < 0:
+      G = -G
+      h = -h
+   
+   P = GetCVXOPTMatrix(P)
+   q = GetCVXOPTMatrix(q)
+   G = GetCVXOPTMatrix(G)
+   h = GetCVXOPTMatrix(h)
+   cvxopt.solvers.options['maxiters'] = 300
+   cvx_solution = cvxopt.solvers.coneqp(P=P, q=q, G=G, h=h, kktsolver='ldl')
+   loss_value = np.nan
+   if 'optimal' in cvx_solution['status']:
+      loss_value = cvx_solution['primal objective']
+      signal_fit = np.array(cvx_solution['x']).reshape((len(q),))
+   else:
+      print("Warning: CVXOPT did not find an optimal solution")
+
+   loss_value += 0.5*np.dot(y,y)
+   if loss_value < 0: # Handle numerical precision issues
+      loss_value = 0
+   if a == 0.0:
+      x = (x1+x2)/2.0
+   else:
+      x = (signal_fit[0]-b)/a
+   return signal_fit[0], x, loss_value
+
 def FitLineSegmentWithIntersection(signal, i, j, a, b, x1, x2):
    if np.isnan(a) or np.isnan(b):
       return np.nan, np.nan, np.nan, np.inf
 
-   x = np.array(signal.iloc[i:j+1].index)
+   # Special case: if signal.iloc[i:j+1] has one point, then there is no unique optimal
+   # solution. Pick the line going through a*x1+b and signal[i].
+   if i == j:
+      x_point = x1
+      new_a = (signal.iloc[i]-(a*x_point+b))/(signal.index[i]-x_point)
+      new_b = signal.iloc[i] - new_a*signal.index[i]
+      loss_value = 0.0
+      return new_a, new_b, x_point, loss_value
+
+   x = np.array(signal.index[i:j+1])
    y = np.array(signal.iloc[i:j+1])
    x_squared = np.square(x)
    P = np.array([[np.sum(x_squared), np.sum(x)],[np.sum(x), len(x)]]).astype(float)
    q = np.array([-np.dot(x,y), -np.sum(y)]).astype(float)
    G1 = np.array([[-x2, -1], [x1, 1], [-1, 0]]).astype(float)
    h1 = np.array([-a*x2-b, a*x1+b, -a]).reshape(-1,1).astype(float)
-   # Check for redundant constraints and fixup if necessary
-   #if np.linalg.matrix_rank(np.hstack((A1,B1))) < len(B1):
-   #   G1 = np.array([[-x2, -1], [x1, 1]]).astype(float)
-   #   h1 = np.array([-a*x2-b, a*x1+b]).T.astype(float)
    G2 = -G1
    h2 = -h1
    
@@ -110,10 +124,6 @@ def FitLineSegmentWithIntersection(signal, i, j, a, b, x1, x2):
    h1 = GetCVXOPTMatrix(h1)
    G2 = GetCVXOPTMatrix(G2)
    h2 = GetCVXOPTMatrix(h2)
-   #args = [GetCVXOPTMatrix(P), GetCVXOPTMatrix(q)]
-   #args.extend([GetCVXOPTMatrix(G), GetCVXOPTMatrix(h)])
-   #args.extend([GetCVXOPTMatrix(A), GetCVXOPTMatrix(b)])
-   #cvx_solution = cvxopt.solvers.qp(*args)
    cvxopt.solvers.options['maxiters'] = 300
    cvx_solution1 = cvxopt.solvers.coneqp(P=P, q=q, G=G1, h=h1, kktsolver='ldl')
    cvx_solution2 = cvxopt.solvers.coneqp(P=P, q=q, G=G2, h=h2, kktsolver='ldl')
@@ -152,7 +162,7 @@ def DoOptimalFit(input_csv_path):
    print("Beginning profile...")
    statprof.start()
    try:
-      # Dynamic program to find the linear segmented regression
+      # Dynamic program to find the optimal trapezoidal segmented regression
       n = len(time)
       F = np.nan*np.zeros((n, num_segments))
       I = np.zeros((n, num_segments)).astype(int)
@@ -177,18 +187,25 @@ def DoOptimalFit(input_csv_path):
             for i in range(t,j):
                k = I[i-1,t-2]
                if k != 0 and A[k-1,i-1] != A[i-1,j-1]:
-                  (a, b, x, cost) = FitLineSegmentWithIntersection(signal, i-1, j-1, A[i-1,t-2], B[i-1,t-2], max(0,signal.index[i-1]), min(signal.index[n-1],signal.index[i]))
+                  previous_segment_slope = A[i-1,t-2]
+                  if previous_segment_slope == 0.0:
+                     (a, b, x, cost) = FitLineSegmentWithIntersection(signal, i, j-1, A[i-1,t-2], B[i-1,t-2], max(0,signal.index[i-1]), min(signal.index[n-1],signal.index[i]))
+                  else:
+                     a = 0
+                     (b, x, cost) = FitConstantSegmentWithIntersection(signal, i, j-1, A[i-1,t-2], B[i-1,t-2], max(0,signal.index[i-1]), min(signal.index[n-1],signal.index[i]))
+
                   if show_debug_plots and not np.isinf(cost):
                      plt.figure()
                      plt.plot(signal.index[0:j], signal.iloc[0:j], 'bo')
                      new_line_x = np.array(signal.index[i-1:j])
                      new_line_y = a*new_line_x + b
                      plt.plot(new_line_x, new_line_y, 'g--')
-                     right_most_line_x = np.array(signal.index[0:i])
+                     right_most_line_x = np.array(signal.index[0:i+1])
                      right_most_line_y = A[i-1,t-2]*right_most_line_x + B[i-1,t-2]
                      plt.plot(right_most_line_x, right_most_line_y, 'r-')
                      plt.title("T=%d, i=%d, j=%d, Cost of new line: %f"%(t, i, j, cost))
                      plt.show()
+
                   if F[j-1,t-1] > F[i-1,t-2] + cost:
                      F[j-1,t-1] = F[i-1,t-2] + cost
                      A[j-1,t-1] = a
@@ -196,7 +213,7 @@ def DoOptimalFit(input_csv_path):
                      I[j-1,t-1] = i
                      X[j-1,t-1] = x
 
-      # Recover optimal approximation -- BB but how??
+      # Recover optimal approximation
       knots = [n]
       x = [signal.index[n-1]]
       i = n
@@ -207,35 +224,16 @@ def DoOptimalFit(input_csv_path):
          t -= 1
       knots.reverse()
       x.reverse()
-      #x = []
       y = [A[knots[1]-1,0]*x[0] + B[knots[1]-1,0]]
       t = 1
       for i in range(1,len(knots)-1):
-         #x.append(X[knots[i]-1, t-1])
          y.append(A[knots[i]-1,t-1]*x[i] + B[knots[i]-1,t-1])
          t += 1
       y.append(A[knots[-1]-1,t-1]*x[-1] + B[knots[-1]-1,t-1])
-      #opt_points = []
-      #t = num_segments-1
-      #i = n-1
-      #x = X[i,t]
-      #y = A[i,t]*x + B[i,t]
-      #opt_points.append((x,y))
-      #i = int(math.floor(x))
-      #t = t-1
-      #while t > 0:
-      #   t = np.argmin(F[i,:])
-      #   x = X[i,t]
-      #   y = A[i,t]*x + B[i,t]
-      #   opt_points.append((x,y))
-      #   i = int(math.floor(x))
-      #   t = t-1
-      #opt_points = reversed(opt_points)
 
       # Plot results
       plt.figure()
       plt.plot(time, signal, 'bo')
-      #plt.plot(zip(*opt_points), 'r-')
       plt.plot(x, y, 'r-')
       plt.show()
 
